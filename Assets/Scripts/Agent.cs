d8abdb5a284853f0e68810fc3e3660d1632a90bc
emeldi <78385236+emeldi@users.noreply.github.com>
2021-07-22 17:07:48 -0700

    laid out pseudocode for next steps

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//require navmesh agent, character controller
+public class Agent : MonoBehaviour //set up control interface
+{
+
+    //onMove
+    //cast ray from mouse position
+    //round to nearest integer coordinate
+    //assign agent destination
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
